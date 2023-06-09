fe1ab2ed086ef2badea73057ea17d43517267ad8
seiren <seirengames@gmail.com>
2023-06-09 16:58:28 +0900

    wip: generate color define

--- raylib-rs-plain/src/lib.rs
+++ raylib-rs-plain/src/lib.rs
@@ -6,6 +6,7 @@ pub use rl::Texture;
 pub use rl::Texture2D;
 pub use rl::Vector2;
 use std::ffi::CString;
+pub mod color_define;
 
 pub fn init_window(width: ::std::os::raw::c_int, height: ::std::os::raw::c_int, title: &str) {
     let title: CString = CString::new(title).unwrap();
